040b42c92066fb099ce1637e9c4b5d1b932d633e
yunliang <804408086@qq.com>
2024-08-12 17:12:38 +0800

    clean

--- .theme/error-boundary.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
-
-export default () => {
-  const error = useRouteError();
-  return (
-    <h4 style={{ color: 'red', padding: 10, margin: 0 }}>{String(error)}</h4>
-  );
-};
